84b8336d83f09dec688e71a0210d40d2177eba33
SurajTopal <ss913475@gmail.com>
2025-02-05 19:07:57 +0530

    Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function componentName() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [2000]);
   return (
-    <View>
-      <Text>...APP...</Text>
-     </View>
+    <View style={{flex: 1, backgroundColor: 'red'}}>
+      <Text style={{marginTop: 30}}>...APP.......</Text>
+    </View>
   );
 }
