a07ce04ad41bfe151f2732ada2d31c134792e5c7
SurajTopal <ss913475@gmail.com>
2025-03-27 14:30:15 +0530

    App name rename

--- ios/LineupsTests/LineupsTests.m
+++ ios/LineupsTests/LineupsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface highRollerClubTests : XCTestCase
+@interface LineupsTests : XCTestCase
 
 @end
 
-@implementation highRollerClubTests
+@implementation LineupsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
